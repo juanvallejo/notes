2388d00d94dc1f726444357e2b245fb6c579c00f
juanvallejo <juan.vallejo.12@cnu.edu>
2014-03-14 10:26:31 -0400

    added new notes

---
+++ programming-theory/lsp:2:24:14.scm
@@ -0,0 +1,17 @@
+;;apostrophe declares a constant
+'(1 42.0 "a string" (9/5))
+
+;;operations happen like this
+(+ 9 4)
+(- 100 10)
+(/ 100 5) ;;20
+(/ 100 5 2) ;;20 and then divide by 2
+(* 31 3)
+
+;;declare vars
+
+;;declare list
+(list 'a 'b 'c)
+
+;;meging 2 lists: APPEND
+;;(APPEND '(A B) '(C D))
