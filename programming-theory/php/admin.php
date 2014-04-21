52c34dcdad0250c9c766fe17cbfd2f979feea654
juanvallejo <juan.vallejo.12@cnu.edu>
2014-04-21 10:39:29 -0400

    final working version of the project

--- programming-theory/php/admin.php
+++ programming-theory/php/admin.php
@@ -21,12 +21,143 @@ if(isset($_GET['module'])) {
 			echo '<h3>Deleting Information</h3>';
 			echo '<input type="button" value="Delete All" name="deleteAll" id="deleteAll"/>';
 		} else {
-			die("Unauthorized access.");
+			die("You are not currently logged in.");
 		}
 	}
 } else {
 	if(isset($_SESSION['password'])) {
-		die('pass');
+		if(isset($_POST['upload'])) {
+			mysqli_report(MYSQLI_REPORT_STRICT);
+			try {
+				$mysqli = new mysqli('127.0.0.1','uMoviesRoot',$_SESSION['password'],'movies');
+				if(!mysqli_connect_errno()) {
+					if(is_file($_FILES['datafile']['tmp_name']) === true) {
+						$file = fopen($_FILES['datafile']['tmp_name'],'r');
+						
+						$object = array();
+						$object["errors"] = array();
+						$object["movies"] = array();
+						$object["movies"]["added"] = 0;
+						$object["movies"]["excluded"] = 0;
+						$object["actors"] = array();
+						$object["actors"]["added"] = 0;
+						$object["actors"]["excluded"] = 0;
+						$object["directors"] = array();
+						$object["directors"]["added"] = 0;
+						$object["directors"]["excluded"] = 0;
+						$object["directions"] = array();
+						$object["directions"]["added"] = 0;
+						$object["directions"]["excluded"] = 0;
+						$object["performances"] = array();
+						$object["performances"]["added"] = 0;
+						$object["performances"]["excluded"] = 0;
+						$object["length"] = 0;
+
+						while(!feof($file)) {
+							$line = explode(", ",fgets($file));
+
+							$type = $line[0];
+							$name = $line[1];
+							$attr = $line[2];
+							$extra = $line[3];
+
+							$object["directions"]["added"]++;
+							$object["performances"]["added"]++;
+							$object["length"]++;
+
+							if($type == 'movie') {
+								$sql = $mysqli->query("SELECT * FROM `movies` WHERE name = '$name'");
+								$object["directions"]["movie"] = $attr;
+								$object["performances"]["movie"] = $attr;
+								if($sql->num_rows == 0) {
+									if($mysqli->query("INSERT INTO `movies` (name,year) VALUES ('$name','$attr')")) {
+										$object["movies"]["last_added"] = $name;
+										$object["movies"]["added"]++;
+									} else {
+										$object['errors'][count($object['errors'])] = mysqli_error($mysqli);
+									}
+								} else {
+									$object["movies"]["excluded"]++;
+								}
+							} elseif($type == 'actor' || $type == 'actress') {
+								$sql = $mysqli->query("SELECT * FROM `actors` WHERE name = '$attr'");
+								$object["directions"]["actor"] = $attr;
+								$object["performances"]["actor"] = $attr;
+								$object["performances"]["role"] = $extra;
+								if($sql->num_rows == 0) {
+									$gender = $type == 'actor' ? 'male' : 'female';
+									try {
+										$mysqli->query("INSERT INTO `actors` (name,gender) VALUES ('$attr','$gender')");
+										$object["actors"]["last_added"] = $attr;
+										$object["actors"]["added"]++;
+										$mysqli->query("INSERT INTO `performed_in` (actor,movie,role) VALUES ('$attr','$name','$extra')");
+									} catch(Exception $e) {
+										$object['errors'][count($object['errors'])] = mysqli_error($mysqli);
+									}
+								} else {
+									//to count the number of unique records, check for repeating roles
+									$mysqli->query("INSERT INTO `performed_in` (actor,movie,role) VALUES ('$attr','$name','$extra')");
+									$object["actors"]["excluded"]++;
+								}
+							} elseif($type == 'director') {
+								$sql = $mysqli->query("SELECT * FROM `directors` WHERE name = '$attr'");
+								$object["directions"]["director"] = $attr;
+								if($sql->num_rows == 0) {
+									try {
+										$mysqli->query("INSERT INTO `directors` (name) VALUES ('$attr')");
+										$object["directors"]["last_added"] = $attr;
+										$object["directors"]["added"]++;
+										$mysqli->query("INSERT INTO `directed_by` (movie,director) VALUES ('$name','$attr')");
+									} catch(Exception $e) {
+										$object['errors'][count($object['errors'])] = mysqli_error($mysqli);
+									}
+								} else {
+									$mysqli->query("INSERT INTO `directed_by` (movie,director) VALUES ('$name','$attr')");
+									$object["directors"]["excluded"]++;
+								}
+							}
+						}
+
+						fclose($file);
+
+						$json = json_encode($object,JSON_FORCE_OBJECT);
+						die($json);
+					}
+				}
+			} catch(Exception $e) {
+				die('ERR_MYSQLI_ERROR\n'.var_dump($e));
+			}
+		} elseif(isset($_POST['deleteAll'])) {
+			mysqli_report(MYSQLI_REPORT_STRICT);
+			try {
+				$mysqli = new mysqli('127.0.0.1','uMoviesRoot',$_SESSION['password'],'movies');
+				if(!mysqli_connect_errno()) {
+					$query = $mysqli->query("SELECT * FROM `movies`");
+
+					if($query->num_rows > 0) {
+						$actors = $mysqli->query("TRUNCATE table `actors`");
+						$db = $mysqli->query("TRUNCATE table `directed_by`");
+						$dirs = $mysqli->query("TRUNCATE table `directors`");
+						$movies = $mysqli->query("TRUNCATE table `movies`");
+						$pi = $mysqli->query("TRUNCATE table `performed_in`");
+					} else {
+						die("success_norecords");
+					}
+
+					if($actors && $db && $dirs && $movies && $pi) {
+						die("success");
+					} else {
+						die(mysqli_error($mysqli));
+					}
+				}
+			} catch(Exception $e) {
+
+			}
+		} else {
+			die('ERR_INVALID_COMMAND');
+		}
+	} else {
+		die('ERR_NOT_LOGGED_IN');
 	}
 }
 ?>
--- programming-theory/php/data.js
+++ programming-theory/php/data.js
@@ -61,16 +61,42 @@ window.addEventListener('load',function() {
 			},
 			uploadForm:function() {
 				if(document.uploadForm) {
-					var logout = document.createElement("a");
-					logout.innerHTML = "Logout";
-					logout.style.cursor = "pointer";
-					logout.addEventListener('click',function() {
-						this.remove();
-						Admin.session.destroy(function() {
-							Admin.load.loginForm();
+					if(!document.getElementById("logout")) {
+						var logout = document.createElement("a");
+						logout.id = "logout";
+						logout.innerHTML = "Logout";
+						logout.style.cursor = "pointer";
+						logout.addEventListener('click',function() {
+							this.remove();
+							Admin.session.destroy(function() {
+								Admin.load.loginForm();
+							});
 						});
+						document.getElementById("links").appendChild(logout);
+					
+					}
+					document.getElementById("deleteAll").addEventListener('click',function() {
+						if(confirm("All data will be deleted. Proceed?")) {
+							var self = this;
+							this.disabled = true;
+							var xhr = new XMLHttpRequest();
+							xhr.open('POST','admin.php',true);
+							xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+							xhr.send("deleteAll=true");
+							xhr.addEventListener('readystatechange',function() {
+								if(this.readyState == 4 && this.status == 200) {
+									self.removeAttribute('disabled');
+									if(this.responseText == "success") {
+										out.innerHTML = "All data deleted!";
+									} else if(this.responseText == "success_norecords") {
+										out.innerHTML = "No records were found to delete.";
+									} else {
+										out.innerHTML = this.responseText;
+									}
+								}
+							});
+						}
 					});
-					document.getElementById("links").appendChild(logout);
 
 					document.uploadForm.submit.addEventListener('click',function(e) {
 						e.preventDefault();
@@ -79,7 +105,53 @@ window.addEventListener('load',function() {
 							alert("The file name cannot be empty");
 						} else {
 							if(confirm("Upload data file\n"+document.uploadForm.dataFile.value+"?")) {
-								
+								var data = new FormData();
+								data.append('upload','true');
+								data.append('datafile',document.uploadForm.dataFile.files[0]);
+
+								var xhr = new XMLHttpRequest();
+								xhr.open('POST','admin.php',true);
+								xhr.send(data);
+								xhr.upload.addEventListener('progress',function(e) {
+
+								});
+								xhr.upload.addEventListener('load',function() {
+									console.log('file upload terminated');
+								});
+								xhr.upload.addEventListener('error',function() {
+
+								});
+								xhr.addEventListener('readystatechange',function() {
+									if(this.status == 200 && this.readyState == 4) {
+										try {
+											var object = JSON.parse(this.responseText);
+											console.log(object);
+											var con = document.getElementById('adminContent');
+											con.innerHTML = '<h3>Uploading data file</h3>';
+											con.innerHTML += '<p>';
+											con.innerHTML += '<ul>';
+											con.innerHTML += '<li>Added <b>'+object.movies.added+'</b> movies out of '+(object.movies.added+object.movies.excluded)+' movie records ('+object.movies.excluded+' failures) [Last added: '+object.movies.last_added+']</li>';
+											con.innerHTML += '<li>Added <b>'+object.actors.added+'</b> actors out of '+(object.actors.added+object.actors.excluded)+' actor records ('+object.actors.excluded+' failures) [Last added: '+object.actors.last_added+']</li>';
+											con.innerHTML += '<li>Added <b>'+object.directors.added+'</b> directors out of '+(object.directors.added+object.directors.excluded)+' director records ('+object.directors.excluded+' failures) [Last added: '+object.directors.last_added+']</li>';
+											con.innerHTML += '<li>Added <b>'+object.directions.added+'</b> directions out of '+(object.directions.added+object.directions.excluded)+' movie / director records ('+object.directions.excluded+' failures) [Last added: '+object.directions.movie+'/'+object.directions.director+']</li>';
+											con.innerHTML += '<li>Added <b>'+object.performances.added+'</b> performances out of '+object.length+' actor / movie / role records ('+object.performances.excluded+' failures) [Last added: '+object.performances.actor+'/'+object.performances.movie+'/'+object.performances.role+']</li>';
+											con.innerHTML += '</ul>';
+											con.innerHTML += '</p>';
+
+											var back = document.createElement("input");
+											back.type = "button";
+											back.value = "Back to Administrator Menu";
+											back.addEventListener('click',function() {
+												Admin.load.uploadForm();
+											});
+
+											con.appendChild(back);
+										} catch(e) {
+											console.log(e.message);
+											console.log(this.responseText);
+										}
+									}
+								});
 							}
 						}
 					});
